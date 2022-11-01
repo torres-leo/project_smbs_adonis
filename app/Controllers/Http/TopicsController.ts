4aed5e5e0c06c949efe5c97120539195ec3cc11f
Leo Torres <leojts24@gmail.com>
2022-11-01 17:12:23 -0600

    add validators

--- app/Controllers/Http/TopicsController.ts
+++ app/Controllers/Http/TopicsController.ts
@@ -1,59 +1,55 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Topic from "App/Models/Topic";
-// import log4js from "log4js"
 import { logger } from '../../../helpers/default.logger';
-import { schema, rules } from "@ioc:Adonis/Core/Validator"
+import { topicSchemaValidator } from '../../../validations';
 
 export default class TopicsController {
 
   public async getTopics({ request }) {
-    // const log = log4js.getLogger("topics")
 
     const page = request.input('page', 1)
     const limit = request.input('limit', 20)
     const title = request.input("title")
-    let topics
     if (title) {
-      topics = await Topic.query().where("title", title)
+      return await Topic.query().where("title", title)
     } else {
-      topics = await Topic.query().paginate(page, limit)
+      return await Topic.query().paginate(page, limit)
     }
-    return topics
   }
 
-  public async getTopic({ params, }: HttpContextContract) {
+  public async getTopic({ params, response }: HttpContextContract) {
     try {
       const topic = await Topic.findOrFail(params.id)
       return topic
     } catch (error) {
-      if (error == "Exception: E_ROW_NOT_FOUND: Row not found") return logger.info(`Topic with id: ${params.id} was Not Found. Try with other ID.`)
-      logger.info(error)
+      response.status(404).send("Failed to get Topic", error)
+      logger.info("Failed to get Topic:", error)
     }
   }
 
-  public async create({ request }: HttpContextContract) {
-    const { title, description } = request.body()
-    const newTopic = schema.create({ title: schema.string([rules.maxLength(50)]), description: schema.string([rules.maxLength(250)]) })
+  public async create({ request, response }: HttpContextContract) {
     try {
-      Topic.create({ title, description })
-      // return response.created("Topic Created.")
+      const payload = await request.validate({ schema: topicSchemaValidator })
+      await Topic.create(payload)
     }
     catch (error) {
-      logger.error('Failed to create Topic', error)
+      response.badRequest(error.messages)
+      logger.error('Failed to create Topic... ', error.messages)
     }
   }
 
+
   public async update({ request, params, response }: HttpContextContract) {
+    // topic.description = request.input("description")
+    // topic.title = request.input("title")
     try {
+      const payload = await request.validate({ schema: topicSchemaValidator })
       const topic = await Topic.findOrFail(params.id)
-      topic.title = request.input("title")
-      topic.description = request.input("description")
-      topic.save()
+      topic.merge(payload)
+      await topic.save()
     } catch (error) {
-      if (error == "Exception: E_ROW_NOT_FOUND: Row not found") return logger.info(`Topic with id: ${params.id} was Not Found. Try with other ID.`)
-      response.badRequest(error.messages)
-      // logger.info(error)
-      // logger.info("Failed to update Topic", error)
+      response.badRequest({ message: `Failed to get Topic: ${params.id}`, error: error.message })
+      logger.error(error)
     }
   }
 
@@ -63,8 +59,13 @@ export default class TopicsController {
       topic.delete()
       return response.status(202).send("Topic deleted")
     } catch (error) {
-      if (error == "Exception: E_ROW_NOT_FOUND: Row not found") return logger.info(`Topic with id: ${params.id} was Not Found. Try with other ID.`)
-      logger.info(error)
+      // if (error == "Exception: E_ROW_NOT_FOUND: Row not found") {
+      //   logger.info(`Topic with id: ${params.id} was Not Found. Try with other ID.`)
+      //   response.badRequest(error)
+      //   return
+      // }
+      logger.error(error)
+      response.badRequest(error)
     }
   }
 }
--- validations/index.ts
+++ validations/index.ts
@@ -1,17 +1,25 @@
-import Joi from "joi";
+// import Joi from "joi";
+// import { logger } from "../helpers/default.logger";
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
-export const topicSchema = Joi.object().keys({
-  title: Joi.string().required().max(50),
-  description: Joi.string().max(250)
+// export const topicSchema = Joi.object().keys({
+//   title: Joi.string().required().max(50),
+//   description: Joi.string().max(250)
+// })
+
+// export const validateSchema = (schema) => {
+//   return async ({ request }, next) => {
+//     try {
+//       await schema.validateAsync(request.body);
+//       next()
+//     } catch (error) {
+//       console.log(error);
+//       logger.error(error)
+//     }
+//   };
+// };
+
+export const topicSchemaValidator = schema.create({
+  title: schema.string({ trim: true }, [rules.maxLength(50), rules.required()]),
+  description: schema.string([rules.maxLength(250), rules.required()])
 })
-
-export const validateSchema = (schema) => {
-  return async (req, res, next) => {
-    try {
-      await schema.validateAsync(req.body);
-      next();
-    } catch (error) {
-      res.send(error.message);
-    }
-  };
-};
